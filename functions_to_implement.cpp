c1948713bbbd4fe2307b9c133db5fbd582bfafa1
richardr1126 <richardr1126@gmail.com>
2022-02-08 13:35:50 -0700

    fixed function

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -130,7 +130,7 @@ double Sum(std::vector<double> nums);
 
 // Multiplies all numbers in a vector together and returns the resulting value
 double Product(std::vector<double> nums) {
-    int product = 0;
+    int product = 1;
     for (int i = 0; i < nums.size(); i++ ) {
         product = product * nums[i];
     }
